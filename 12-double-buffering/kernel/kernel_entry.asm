026c86b342c5672aeb1b15c5e6cf3ad737d86fb3
cvanehcke <clement.van-hecke@netatmo.com>
2021-04-04 00:44:46 +0200

    set pit frequency

--- 12-double-buffering/kernel/kernel_entry.asm
+++ 12-double-buffering/kernel/kernel_entry.asm
@@ -41,6 +41,19 @@ out dx, al
 lidt [idt_descriptor]
 sti
 
+; PIT set 100Hz
+mov edx, 0x43 ; Command Register
+mov al, 0x36 ; 0b00110110 => |CNTR=0(Counter)|RW=3(LSB then MSB)|MODE=3(square wave mode)|BCD=0(false)|
+out dx, al
+
+mov edx, 0x40 ; Data register
+mov al, (1193180 / 100) & 0xFF ; LSB
+out dx, al
+
+mov edx, 0x40 ; Data register
+mov al, (1193180 / 100) >> 8 ; MSB
+out dx, al
+
 mov edx, 0x21
 mov al, 0xFC
 out dx, al
